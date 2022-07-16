8a5f7457c6b7c1ac6993f5fa9abab520b0b0f298
Michael Pascuzzi <gatorpazz@gmail.com>
2022-07-16 13:11:17 -0400

    Made cr edits

--- howManyHours.js
+++ howManyHours.js
@@ -1,6 +1,6 @@
 const howManyHours = (hours) => {
   console.log("Dad how many hours has it been?");
-  console.log(`It has been ${hours} hours`);
+  return `It has been ${hours} hours`;
 }
 
-console.log(howManyHours(5));
+export default howManyHours;
